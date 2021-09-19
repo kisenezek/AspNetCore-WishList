d6d5efde3986843a53e707473c8ae6a9fba77fb0
kisenezek <46268932+kisenezek@users.noreply.github.com>
2021-09-19 12:17:59 +0200

    module

--- WishList/Program.cs
+++ WishList/Program.cs
@@ -18,7 +18,7 @@ namespace WishList
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
     }
